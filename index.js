b64356a1adce8f3cc0cd2a4f1c195e5bbe136a19
hkgnp <ben@pngs.cc>
2022-07-22 01:11:04 +0800

    Added results profile. Next is dashboard.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const mongoUrl = process.env.MONGO_URL;
 const MongoUtil = require("./MongoUtil");
+const ObjectID = require("mongodb").ObjectID;
 const hbs = require("hbs");
 const wax = require("wax-on");
 
@@ -15,6 +16,18 @@ app.set("view engine", "hbs");
 wax.on(hbs.handlebars);
 wax.setLayoutPath("./views/layouts");
 
+// if equal function hbs helper
+hbs.registerHelper("if_eq", (a, b, options) => {
+  if (a === b) return options.fn(this);
+  else return options.inverse(this);
+});
+
+hbs.registerHelper("eq", function () {
+  const args = Array.prototype.slice.call(arguments, 0, -1);
+  return args.every(function (expression) {
+    return args[0] === expression;
+  });
+});
 // enable forms
 app.use(
   express.urlencoded({
@@ -36,14 +49,47 @@ app.use(
   });
 
   app.post("/", async (req, res) => {
-    console.log(req.body);
-    //const { qn12 } = req.body;
-    //const results = await db.collection("results").insertOne({
-    //  qn12: qn12,
-    //});
+    const { qn1, qn2, qn3, qn4, qn5, qn6, qn7, qn8, qn9, qn10, qn11, qn12 } =
+      req.body;
+
+    const screenScore =
+      parseInt(qn1) + parseInt(qn2) + parseInt(qn5) + parseInt(qn10);
+
+    const surveyScore =
+      parseInt(qn1) +
+      parseInt(qn2) +
+      parseInt(qn3) +
+      parseInt(qn4) +
+      parseInt(qn5) +
+      parseInt(qn6) +
+      parseInt(qn7) +
+      parseInt(qn8) +
+      parseInt(qn9) +
+      parseInt(qn10) +
+      parseInt(qn11) +
+      parseInt(qn12);
+
+    let postObj = req.body;
+    postObj["screenScore"] = screenScore;
+    postObj["surveyScore"] = surveyScore;
+
+    const result = await db.collection("results").insertOne(postObj);
+    const id = result.insertedId;
+
     res.status(200);
-    //console.log(results);
-    res.redirect("/");
+    res.redirect(`/results/${id}`);
+  });
+
+  app.get("/results/:id", async (req, res) => {
+    const result = await db
+      .collection("results")
+      .find({ _id: new ObjectID(req.params.id) })
+      .toArray();
+
+    res.status(200);
+    res.render("profile", {
+      result: result[0],
+    });
   });
 
   // Dashboard page
